c0389497119eaa159b2baa5e381426c449608372
Robert Rondon <robrondon11@gmail.com>
2024-03-24 00:39:22 -0300

    Exercise 1.8 - Statistics Component and refactor

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -3,41 +3,42 @@ import { useState } from 'react'
 const Button = ({ onClick, text }) => (<button onClick={onClick}>{text}</button>)
 const Title = ({ text }) => (<h2>{text}</h2>)
 const Display = ({ text, value }) => (<p>{text} {value}</p>)
+const Statistics = ({ good, neutral, bad, total, average, positive }) => {
+  return (
+    <>
+      <Title text={'Statistics'} />
+      <Display text={'good'} value={good} />
+      <Display text={'neutral'} value={neutral} />
+      <Display text={'bad'} value={bad} />
+      <Display text={'all'} value={total} />
+      <Display text={'average'} value={average} />
+      <Display text={'positive'} value={`${positive}%`} />
+    </>
+  )
+}
 
 function App () {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
   const [total, setTotal] = useState(0)
-  const [average, setAverage] = useState(0)
   const [points, setPoints] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const average = total === 0 ? 0 : points / total
+  const positive = total === 0 ? 0 : (good / total) * 100
 
   const handleGoodButton = () => {
-    const newPoints = points + 1
-    const newTotal = total + 1
-    const newGood = good + 1
-    setGood(newGood)
-    setTotal(newTotal)
-    setPoints(newPoints)
-    setAverage(newPoints / newTotal)
-    setPositive((newGood * 100) / newTotal)
+    setGood(good + 1)
+    setTotal(total + 1)
+    setPoints(points + 1)
   }
   const handleNeutralButton = () => {
-    const newTotal = total + 1
     setNeutral(neutral + 1)
-    setTotal(newTotal)
-    setAverage(points / newTotal)
-    setPositive((good * 100) / newTotal)
+    setTotal(total + 1)
   }
   const handleBadButton = () => {
-    const newPoints = points - 1
-    const newTotal = total + 1
     setBad(bad + 1)
-    setTotal(newTotal)
-    setPoints(newPoints)
-    setAverage(newPoints / newTotal)
-    setPositive((good * 100) / newTotal)
+    setTotal(total + 1)
+    setPoints(points - 1)
   }
 
   return (
@@ -46,13 +47,7 @@ function App () {
       <Button onClick={handleGoodButton} text={'good'} />
       <Button onClick={handleNeutralButton} text={'neutral'} />
       <Button onClick={handleBadButton} text={'bad'} />
-      <Title text={'Statistics'} />
-      <Display text={'good'} value={good} />
-      <Display text={'neutral'} value={neutral} />
-      <Display text={'bad'} value={bad} />
-      <Display text={'all'} value={total} />
-      <Display text={'average'} value={average} />
-      <Display text={'positive'} value={`${positive}%`} />
+      <Statistics good={good} neutral={neutral} bad={bad} total={total} average={average} positive={positive} />
     </>
   )
 }
