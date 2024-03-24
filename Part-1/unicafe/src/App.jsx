45accfc56e9dbe6d28ba1ee09ec91dc01c15efef
Robert Rondon <robrondon11@gmail.com>
2024-03-24 00:08:51 -0300

    Excercise 1.7

--- Part-1/unicafe/src/App.jsx
+++ Part-1/unicafe/src/App.jsx
@@ -8,10 +8,37 @@ function App () {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const [total, setTotal] = useState(0)
+  const [average, setAverage] = useState(0)
+  const [points, setPoints] = useState(0)
+  const [positive, setPositive] = useState(0)
 
-  const handleGoodButton = () => setGood(good + 1)
-  const handleNeutralButton = () => setNeutral(neutral + 1)
-  const handleBadButton = () => setBad(bad + 1)
+  const handleGoodButton = () => {
+    const newPoints = points + 1
+    const newTotal = total + 1
+    const newGood = good + 1
+    setGood(newGood)
+    setTotal(newTotal)
+    setPoints(newPoints)
+    setAverage(newPoints / newTotal)
+    setPositive((newGood * 100) / newTotal)
+  }
+  const handleNeutralButton = () => {
+    const newTotal = total + 1
+    setNeutral(neutral + 1)
+    setTotal(newTotal)
+    setAverage(points / newTotal)
+    setPositive((good * 100) / newTotal)
+  }
+  const handleBadButton = () => {
+    const newPoints = points - 1
+    const newTotal = total + 1
+    setBad(bad + 1)
+    setTotal(newTotal)
+    setPoints(newPoints)
+    setAverage(newPoints / newTotal)
+    setPositive((good * 100) / newTotal)
+  }
 
   return (
     <>
@@ -23,6 +50,9 @@ function App () {
       <Display text={'good'} value={good} />
       <Display text={'neutral'} value={neutral} />
       <Display text={'bad'} value={bad} />
+      <Display text={'all'} value={total} />
+      <Display text={'average'} value={average} />
+      <Display text={'positive'} value={`${positive}%`} />
     </>
   )
 }
