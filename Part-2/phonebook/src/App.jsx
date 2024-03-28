d67667d33d2070af049f588aeb41d225ad34559d
Robert Rondon <robrondon11@gmail.com>
2024-03-28 16:43:48 -0300

    Exercise 2.8 adding phone numbers

--- Part-2/phonebook/src/App.jsx
+++ Part-2/phonebook/src/App.jsx
@@ -2,19 +2,28 @@ import { useState } from 'react'
 
 function App () {
   const [persons, setPersons] = useState([
-    { name: 'Rob Rondon' }
+    { name: 'Rob Rondon', number: '+5491112345678' }
   ]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
+
   const addName = (e) => setNewName(e.target.value)
+  const addNumber = (e) => setNewNumber(e.target.value)
+
   const handleSubmit = (e) => {
     e.preventDefault()
+
     if (newName.trim() === '') return
     if (checkIfNameExists(newName.trim())) return alert(`${newName} is already added to the phonebook`)
+
     const newPerson = {
-      name: newName.trim()
+      name: newName.trim(),
+      number: newNumber.trim()
     }
     setPersons(persons.concat(newPerson))
+
     setNewName('')
+    setNewNumber('')
   }
   const checkIfNameExists = (name) => persons.some(p => p.name.toLowerCase() === name.toLowerCase())
 
@@ -26,11 +35,14 @@ function App () {
           name: <input value={newName} onChange={addName} />
         </div>
         <div>
+          number: <input value={newNumber} onChange={addNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map(person => (<p key={person.name}>{person.name}</p>))}
+      {persons.map(person => (<p key={person.name}>{person.name} {person.number}</p>))}
     </div>
   )
 }
